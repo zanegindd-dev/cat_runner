58535d4f31feff8da839cad57be0c53d1d7b4749
Daniil <zanegindd@gmail.com>
2026-01-31 14:19:41 +0700

    init

---
+++ game.js
@@ -0,0 +1,16 @@
+﻿let cat = document.querySelector('.cat')
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Space') {
+    console.log('Пробел нажат');
+
+	if([...cat.classList].includes('jump')){
+		cat.classList.remove('jump');
+
+	}
+	else{
+		cat.classList.add('jump');
+	}
+    	
+	
+  }
+});
